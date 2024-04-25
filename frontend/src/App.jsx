987833f835d3b81b2169c0ede551a7c64907f205
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-04-25 22:32:50 +0545

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+ 
+  return(
+    <>
+  <p>hello</p>
+     {/* <Navbar/> */}
+
+     <Drawer>
+  <DrawerTrigger>Open</DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+    </DrawerHeader>
+    <DrawerFooter> 
+      <DrawerClose>
+      </DrawerClose>
+    </DrawerFooter>
+  </DrawerContent>
+</Drawer>
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  Drawer,
+ 
+} from "@/components/ui/drawer"
+
+
+const Navbar = () => {
+  return (
+    <div className=' text-black bg-purple-400 h-[12rem]'>
+
+
+    </div>
+  )
+}
+
+export default Navbar
