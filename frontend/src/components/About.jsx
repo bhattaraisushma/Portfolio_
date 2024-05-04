bd70b093d26bc1a2a0d7d3efd35f79bf28a9f8a7
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-05-04 09:35:06 +0545

    some responsove changes

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const About = () => {
   return (
     <div className='md:h-screen h-fit w-full flex md:flex-row flex-col justify-between  text-white md:px-[4rem] font-montserrat' id='/about'>
-    <div className='flex md:flex-row flex-col  items-center md:w-[50%] w-full p-4 justify-center'>
-<h1 className='md:text-4xl text-2xl font-bold pb-4'>    Who am  I? </h1>
+    <div className='flex  md:flex-col  items-center md:w-[50%] w-full p-4 justify-center'>
+<h1 className='md:text-4xl text-2xl font-bold md:pb-4'>    Who am  I? </h1>
 <h2 className='md:text-2xl text-xl font-medium pb-6  w-fit'>I am Sushma Bhattarai with keen interest in web development and Graphics Designing currently pursuing Computer Engineering in Thapathali Campus, Kathmandu</h2>
 <h2 className='md:text-2xl text-xl md:font-medium font-normal'>I have a ultimate passion for code as a web developer. My passion and undoubted dedication towards tech was enlightened on me seeing the prospects and vast potentiality in my country.
 Creating multiple webs and serving in the Tech sectors always keeps me motivated. 
 From tinkering with hardware to delving into the intricacies of software development, I've explored every corner of the tech universe. My quest for knowledge has led me to embrace emerging trends, dive deep into coding languages, and stay ahead of the curve in an ever-evolving digital landscape.
 </h2></div>
     {/* <div className=''> */}
-      <div className='h-[40rem] md:w-[30rem]  flex justify-center items-center bg-red'>
+      <div className='h-[40rem] md:w-[30rem]  flex justify-center md:items-center bg-red'>
       <img src='./sushma.jpg' className='rounded-3xl h-[30rem]'></img>
       {/* </div> */}
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,17 +14,17 @@ import Skill from './Skill'
 const Home = () => {
   return (
     <>
-         <div className='bg-bgimg md:h-screen h-fit w-screen bg-cover md:mb-0 mb-[14rem]  bg-no-repeat text-white' id ='/'>
+         <div className='bg-bgimg md:h-screen h-fit w-screen bg-cover md:mb-0 mb-[14rem]  bg-no-repeat  text-white' id ='/'>
     
-    <div className="bg-[#000000] bg-opacity-[80%] md:h-screen h-fit w-screen  md:pl-[10rem]" >
+    <div className="bg-[#000000] bg-opacity-[80%] md:h-screen h-fit w-screen  md:pr-[10rem]" >
   
 <div className='flex md:flex-row flex-col md:justify-between gap-2 md:items-center md:h-full w-screen h-fit pt-[6rem]  '>
     
-    <div className='flex flex-col md:gap-9 gap-3 md:w-[50%] w-full md:pr-[5rem] items-center'>
+    <div className='flex flex-col md:gap-9 gap-3 md:w-[50%] w-full  items-center'>
     <h1  className='md:text-5xl text-3xl font-semibold'>Sushma Bhattarai</h1>
     <h2 className='md:text-3xl text-xl mb-6'>Web developer /Graphics Desinger</h2>
       </div> 
-     <div className=' flex justify-center items-center   md:h-full md:w-[50%] w-full  '>
+     <div className='  flex justify-center items-center   md:h-screen md:w-[50%] w-full  '>
     <div className=''>
      <Carousel  plugins={[
         Autoplay({
