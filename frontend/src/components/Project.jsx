32e7ccaaeff4e660758a7283e2c714e037ceca07
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-05-24 20:30:26 +0545

    projects

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {projects} from './data'
 const Project = () => {
   return (
-    <div id='/project' className='md:h-screen w-full flex md:flex-row justify-center md:gap-[16rem] gap-[6rem] flex-col p-[6rem] md:top-0 top-[4rem]'>
+    <div id='/project' className='md:h-screen w-full flex md:flex-row justify-center md:gap-[16rem] gap-[6rem] flex-col md:p-[6rem] p-6 md:top-0 top-[4rem]'>
+   <div className='font-montserrat text-3xl'>My Projects </div>
     {projects.map((project,index)=>(
 <div id={index} className='md:h-[35rem] h-fit md:w-[25rem]  w-fit p-3 font-montserrat flex flex-col gap-4 text-xl border-solid border-2 text-justify  rounded-[2rem] shadow-md shadow-white'>
     <h1 className='font-semibold text-3xl text-center'>{project.Title}</h1>
--- frontend/src/components/Skill.jsx
+++ frontend/src/components/Skill.jsx
@@ -10,8 +10,11 @@ import { Progress } from "@/components/ui/progress"
 const Skill = () => {
   return (
     <div className='md:h-screen h-fit w-full text-3xl gap-[5rem] flex    bg-bgimg bg-cover bg-no-repeat font-montserrat text-white' id='/skill'>
+    
     <div className='bg-[#000000] bg-opacity-[85%] md:h-screen h-fit w-full  flex justify-start md:p-[5rem] md:items-center md:top-0 pt-[16rem]'>
+
     <div  className=' h-[30rem] w-[40rem rounded-2xl p-5 '>
+    <div className='font-montserrat text-3xl flex justify-center'>My Skills </div>
     <Accordion type="single" collapsible>
   <AccordionItem value="item-1">
     <AccordionTrigger>Html</AccordionTrigger>
