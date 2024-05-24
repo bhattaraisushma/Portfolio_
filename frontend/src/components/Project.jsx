5bbc923884835a6b6553e5fdfdd058fb60298842
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-05-24 20:22:56 +0545

    projects

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {projects} from './data'
 const Project = () => {
   return (
-    <div id='/project' className='h-screen w-full bg-slate-50'>
-      hello
+    <div id='/project' className='md:h-screen w-full flex md:flex-row justify-center md:gap-[16rem] gap-[6rem] flex-col p-[6rem] md:top-0 top-[4rem]'>
+    {projects.map((project,index)=>(
+<div id={index} className='md:h-[35rem] h-fit md:w-[25rem]  w-fit p-3 font-montserrat flex flex-col gap-4 text-xl border-solid border-2 text-justify  rounded-[2rem] shadow-md shadow-white'>
+    <h1 className='font-semibold text-3xl text-center'>{project.Title}</h1>
+    <h1>{project.Description}</h1>
+    <img src={project.image} className='my-10'></img>
+    <a href={project.link}>Visit Website</a>
+
+</div>
+    ))}
     </div>
   )
 }
---
+++ frontend/src/components/data.js
@@ -0,0 +1,16 @@
+
+
+
+ export const  projects = [{
+    Title: "Ragat Nepal",
+    Description: "A website where you can search for blood donors and request for blood donation. You can search for location too.",
+    image:"public/ragatnepal.png",
+   
+    link:"https://ragat-nepal-1edt-git-master-bhattaraisushmas-projects.vercel.app/"
+},
+{
+    Title:"Furnitech",
+    Description: "A website where you can buy furniture online.",
+    image:'/furnitech.png',
+    link:"https://e-commerce-q8j8hu206-bhattaraisushmas-projects.vercel.app/"
+}]
