1c56d4e4729ed082058b4edb81d4fa3cde767bf3
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-05-24 20:32:03 +0545

    projects

--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -4,7 +4,7 @@
  export const  projects = [{
     Title: "Ragat Nepal",
     Description: "A website where you can search for blood donors and request for blood donation. You can search for location too.",
-    image:"public/ragatnepal.png",
+    image:"/ragatnepal.png",
    
     link:"https://ragat-nepal-1edt-git-master-bhattaraisushmas-projects.vercel.app/"
 },
