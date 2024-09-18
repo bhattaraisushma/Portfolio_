f30cade4d7a5f00fb6ef6c738012671ad382b336
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-09-18 20:45:22 +0545

    project added

--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -1,7 +1,14 @@
 
 
 
- export const  projects = [{
+ export const  projects = [
+    {
+        Title:"Furnitech",
+        Description: "A website where you can buy furniture online.",
+        image:'/furnitech.png',
+        link:"https://e-commerce-git-master-bhattaraisushmas-projects.vercel.app/"
+    },
+    {
     Title: "Ragat Nepal",
     Description: "A website where you can search for blood donors and request for blood donation. You can search for location too.",
     image:"/ragatnepal.png",
@@ -9,8 +16,11 @@
     link:"https://ragat-nepal-1edt-git-master-bhattaraisushmas-projects.vercel.app/"
 },
 {
-    Title:"Furnitech",
-    Description: "A website where you can buy furniture online.",
-    image:'/furnitech.png',
-    link:"https://e-commerce-git-master-bhattaraisushmas-projects.vercel.app/"
-}]
+    Title:"Blogging Site",
+    images:"blog.png",
+    Description:"CURRENTLY UNDER DEVELOPMENT. A website where you can write blogs and read blogs.",
+    link:"https://blogging-git-master-bhattaraisushmas-projects.vercel.app/"
+}
+
+
+]
