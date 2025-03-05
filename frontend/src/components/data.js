103594edea861b0dcae923380fa772437bcad8f1
bhattaraisushma <bhattaraisushma260@gmail.com>
2025-03-05 14:56:21 +0545

    project changes

--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -8,13 +8,13 @@
         image:'/furnitech.png',
         link:"https://e-commerce-git-master-bhattaraisushmas-projects.vercel.app/"
     },
-    {
-    Title: "Ragat Nepal",
-    Description: "A website where you can search for blood donors and request for blood donation. You can search for location too.",
-    image:"/ragatnepal.png",
+//     {
+//     Title: "Ragat Nepal",
+//     Description: "A website where you can search for blood donors and request for blood donation. You can search for location too.",
+//     image:"/ragatnepal.png",
    
-    link:"https://ragat-nepal-1edt-git-master-bhattaraisushmas-projects.vercel.app/"
-},
+//     link:"https://ragat-nepal-1edt-git-master-bhattaraisushmas-projects.vercel.app/"
+// },
 {
     Title:"Blogging Site",
     image:"/blog.png",
